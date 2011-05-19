38059158868fa4447737c68520a745e9482f42a8
Priscila Mayumi Sato <mayumisatox#gmail.com>
2011-05-19 13:47:46 -0300

    Comecando pelo de Estrategia. Ainda nao terminado

---
+++ Strategy/Pessoa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Strategy
+{
+    public abstract class Pessoa
+    {
+        public String nome { get; set; }
+        public Int32 idade { get; set; }
+        public Profissao profissao { get; set; }
+        public void usaHabilidadeEspecial();
+    }
+}
---
+++ Strategy/Profissao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Strategy
+{
+    public interface Profissao
+    {
+        public String nome { get; set; }
+        public Int32 forca { get; set; }
+        public Int32 velocidade { get; set; }
+        public Int32 magia { get; set; }
+
+        public void usaHabilidadeEspecial();
+    }
+
+    public class Mago
+    {
+
+    }
+}
